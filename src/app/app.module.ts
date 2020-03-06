2ef93d5b551da715f65955529fe6290d7cba02ff
Willmer <willmer2911@gmail.com>
2020-03-06 16:28:13 -0400

    Listo la fecha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule, LOCALE_ID } from '@angular/core';
 import { NgbModule, NgbDateParserFormatter, NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
 import localeEsVe from '@angular/common/locales/es-VE';
 import { registerLocaleData } from '@angular/common';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { CustomDateParserFormatter } from './shared/datepicker-formatter';
+import { InputMaskModule } from 'racoon-mask-raw';
 
 registerLocaleData(localeEsVe, 'es-VE');
 
@@ -20,7 +22,8 @@ registerLocaleData(localeEsVe, 'es-VE');
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    FormsModule
+    FormsModule,
+    InputMaskModule
   ],
   providers: [
     { provide: NgbDateParserFormatter, useClass: CustomDateParserFormatter },
